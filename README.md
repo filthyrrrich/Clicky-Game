04854452104b6f54d545abd7dd4700ea3d5bec46
Rich Schiffer <richschiffer@Richs-MacBook-Pro.local>
2018-10-04 18:26:47 -0600

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Clicky-Game
-
-This is a simple memory game app built with React.
---
+++ precache-manifest.b502810e680c7a3b1a79eef4725549b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "26d721bb2ed56eb8bb84",
+    "url": "/Clicky-Game/static/js/main.26d721bb.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/Clicky-Game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "26d721bb2ed56eb8bb84",
+    "url": "/Clicky-Game/static/css/main.60118598.chunk.css"
+  },
+  {
+    "revision": "beb1d7e202e953b1d37662d30e81b98e",
+    "url": "/Clicky-Game/index.html"
+  }
+];
